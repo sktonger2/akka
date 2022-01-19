c8f89f43321b89f0dee1789780eb9ed106329677
sktonger2 <98034742+sktonger2@users.noreply.github.com>
2022-01-19 19:56:45 +0530

    Update AkkaQuickstartSpec.scala
    build failure

--- src/test/scala/com/example/AkkaQuickstartSpec.scala
+++ src/test/scala/com/example/AkkaQuickstartSpec.scala
@@ -13,6 +13,7 @@ class AkkaQuickstartSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike 
 
   "A Greeter" must {
     //#test
+    123444
     "reply to greeted" in {
       val replyProbe = createTestProbe[Greeted]()
       val underTest = spawn(Greeter())
